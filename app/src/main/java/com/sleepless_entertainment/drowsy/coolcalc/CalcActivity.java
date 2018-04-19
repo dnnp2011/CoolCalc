59a0fc467f72a8089afbf32ad5a535b70037bd0b
dnnp2011 <dnnp2011@gmail.com>
2018-04-18 23:52:33 -0400

    Layout v1 complete

--- app/src/main/java/com/sleepless_entertainment/drowsy/coolcalc/CalcActivity.java
+++ app/src/main/java/com/sleepless_entertainment/drowsy/coolcalc/CalcActivity.java
@@ -2,12 +2,87 @@ package com.sleepless_entertainment.drowsy.coolcalc;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CalcActivity extends Activity {
+
+    Button oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nineBtn, zeroBtn, multBtn, divBtn, addBtn, subBtn, clrBtn, eqlBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        oneBtn = findViewById(R.id.numberOne);
+    }
+
+    public void oneClick(View view) {
+
+    }
+
+    public void twoClick(View view) {
+
+    }
+
+    public void threeClick(View view) {
+
+    }
+
+    public void fourClick(View view) {
+
+    }
+
+    public void fiveClick(View view) {
+
+    }
+
+    public void sixClick(View view) {
+
+    }
+
+    public void sevenClick(View view) {
+
+    }
+
+    public void eightClick(View view) {
+
+    }
+
+    public void nineClick(View view) {
+
+    }
+
+    public void zeroClick(View view) {
+
+    }
+
+    public void multClick(View view) {
+
+    }
+
+    public void divClick(View view) {
+
+    }
+
+    public void addClick(View view) {
+
+    }
+
+    public void subClick(View view) {
+
+    }
+
+    public void eqlClick(View view) {
+
+    }
+
+    public void clrClick(View view) {
+
     }
 }
